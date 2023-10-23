a2397379e7bbf53cda04a4758da1084e3f37603d
lebjent <wnsvy2885@gmail.com>
2023-10-23 14:13:43 +0900

    TripManager-first

---
+++ Trip-MANAGE/Trip-Manager/src/main/java/tnew/manager/project/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package tnew.manager.project.config;
+
+	
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+	
+    //Swagger API 최상단에 들어갈 타이틀 설정
+    private static final String API_NAME = "Wedding Bliss Tour Manager API";
+    //버전을 기록하는 설정
+    private static final String API_VERSION = "0.0.1";
+    //그다음 하단에 들어갈 Sub타이틀 설정
+    private static final String API_DESCRIPTION = "웨딩블레스투어 매니저 API";
+	
+    @Bean
+    public GroupedOpenApi publicApi() {
+    	return GroupedOpenApi.builder()
+    		   .group("Version-0.0.1")
+    		   .pathsToMatch("/**")
+    		   .build();
+    }
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+ 
+    private Info apiInfo() {
+        return new Info()
+                .title(API_NAME)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION);
+    }
+    
+}
+
+	
---
+++ Trip-MANAGE/Trip-Manager/src/test/java/tnew/manager/project/TripManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tnew.manager.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
